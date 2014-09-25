08d605d42a5d664b823bf09fc05be2cf5e1203d3
Gomez Roque <rokex_gomex@outlook.com>
2014-09-25 01:05:25 -0300

    Prueba de guardar cambios

--- Trabajo de Laboratorio/src/trabajo/de/laboratorio/TrabajoDeLaboratorio.java
+++ Trabajo de Laboratorio/src/trabajo/de/laboratorio/TrabajoDeLaboratorio.java
@@ -14,6 +14,6 @@ public class TrabajoDeLaboratorio {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("Esto es una prueba");
     }
 }
