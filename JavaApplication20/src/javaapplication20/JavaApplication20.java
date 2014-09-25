2d3532ee57f75476d088501e1323df746387652b
lucianovazquez <lucianovazquez@outlook.com>
2014-09-24 23:31:14 -0300

    Hola mundo 2

--- JavaApplication20/src/javaapplication20/JavaApplication20.java
+++ JavaApplication20/src/javaapplication20/JavaApplication20.java
@@ -15,5 +15,6 @@ public class JavaApplication20 {
      */
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
+        System.out.println("Hola Mundo 2 !");
     }
 }
