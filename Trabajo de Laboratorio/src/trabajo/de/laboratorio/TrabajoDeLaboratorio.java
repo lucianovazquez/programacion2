7fe3fb57123d1ca7688ca6848c8ae465af32653f
Gomez Roque <roqueg@users.noreply.github.com>
2014-09-25 01:27:40 -0300

    Modificando contenido desde la web
    Modificando contenido desde la web para ver si se actualiza solo en el netbeans

--- Trabajo de Laboratorio/src/trabajo/de/laboratorio/TrabajoDeLaboratorio.java
+++ Trabajo de Laboratorio/src/trabajo/de/laboratorio/TrabajoDeLaboratorio.java
@@ -14,6 +14,6 @@ public class TrabajoDeLaboratorio {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         
+        System.out.printf("Hola mundo");
     }
 }
