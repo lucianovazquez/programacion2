d47bd5866ba690133b9edf53d741e4968e393f93
lucianovazquez <lucianovazquez@outlook.com>
2014-10-01 01:59:01 -0300

    Complete atributos de clase Empleado, Creación de clases Vendedor y Jefe, ambas heredan de clase empleado.

--- Trabajo de Laboratorio/src/trabajodelaboratorio/Empleado.java
+++ Trabajo de Laboratorio/src/trabajodelaboratorio/Empleado.java
@@ -4,10 +4,19 @@ package trabajodelaboratorio;
  *
  * @author RoqueG
  */
-public class Empleado extends Persona {
+public abstract class Empleado extends Persona {
     
-        public Empleado(String nombre, String apellido,String calle, int numero, int dni,int diaNac,int mesNac,int añoNac){
+    private String tipo_cargo;
+    private int nro_legajo;
+    private LocalDate fec_ingreso;
+    
+        public Empleado(String nombre, String apellido,String calle, int numero, int dni,int diaNac,int mesNac,int añoNac, String tipo_cargo, int nro_legajo, LocalDate fec_ingreso){
+            
         super(nombre,apellido,calle,numero,dni,diaNac,mesNac,añoNac);
+        this.tipo_cargo=tipo_cargo;
+        this.nro_legajo=nro_legajo;
+        this.fec_ingreso=fec_ingreso;
+        
     }
     
 }
