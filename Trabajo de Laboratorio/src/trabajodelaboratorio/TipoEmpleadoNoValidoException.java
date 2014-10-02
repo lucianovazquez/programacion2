49512d69be83e819816d860f1076ea0bca2e5940
Gomez Roque <rokex_gomex@outlook.com>
2014-10-02 06:22:06 -0300

    Nueva excepcion para los casos en que se ingresa un tipo de empleado no permitido en un objeto jefe

---
+++ Trabajo de Laboratorio/src/trabajodelaboratorio/TipoEmpleadoNoValidoException.java
@@ -0,0 +1,11 @@
+package trabajodelaboratorio;
+
+/**
+ *
+ * @author RoqueG
+ */
+public class TipoEmpleadoNoValidoException extends Exception {
+    public TipoEmpleadoNoValidoException(String s){
+        super(s);
+    }
+}
