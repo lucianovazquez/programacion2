766f1b13ca805d135d093b1394f994f7bf6c494b
Gomez Roque <rokex_gomex@outlook.com>
2014-10-02 07:09:54 -0300

    Creacion objeto vendedor y intento de agregar vendedor a jefe de area para obtener una excepcion

--- Trabajo de Laboratorio/src/trabajodelaboratorio/TrabajoDeLaboratorio.java
+++ Trabajo de Laboratorio/src/trabajodelaboratorio/TrabajoDeLaboratorio.java
@@ -12,6 +12,18 @@ public class TrabajoDeLaboratorio {
      */
     public static void main(String[] args) {
         System.out.println("Hola maldito mundo!!!!!");
+        
+       /* Creacion de un empleado tipo jefe*/
         Empleado empleado1 = new Jefe("Juan","Perez",new Domicilio("Belgrano",520),24503291,LocalDate.of(1990,10,4),483838,LocalDate.of(2010,6,20),"Tecnologia");
+        
+        /* Creacion de un empleado tipo vendedor*/
+        Empleado empleado2 = new Vendedor("Juan","Carlos",new Domicilio("Belgrano",520),24503291,LocalDate.of(1990,10,4),483838,LocalDate.of(2010,6,20),1,20.00f);
+        
+        Jefe jef1=(Jefe)empleado1;
+        try{
+            jef1.addEmpleadoANomina(empleado2);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
