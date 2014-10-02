625a5b8777b7124d7817292a6d4cd105d499e439
Gomez Roque <rokex_gomex@outlook.com>
2014-10-02 14:10:00 -0300

    Nueva clase: empleado Operario

---
+++ Trabajo de Laboratorio/src/trabajodelaboratorio/Operario.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package trabajodelaboratorio;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Net
+ */
+public class Operario extends Empleado{
+
+    public Operario(String nombre, String apellido,char sexo, Domicilio domicilio, int dni, LocalDate fec_nacimiento, int nro_legajo, LocalDate fec_ingreso) {
+        super(nombre,apellido,sexo,domicilio,dni,fec_nacimiento,nro_legajo,fec_ingreso,"Operario");
+    }
+}
