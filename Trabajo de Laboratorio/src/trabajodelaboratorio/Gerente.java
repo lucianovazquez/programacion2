1453874e8c7aca7b5a627cc6c72e31007134bc89
Gomez Roque <rokex_gomex@outlook.com>
2014-10-02 14:08:25 -0300

    Nueva clase: empleado Gerente

---
+++ Trabajo de Laboratorio/src/trabajodelaboratorio/Gerente.java
@@ -0,0 +1,15 @@
+package trabajodelaboratorio;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Net
+ */
+public class Gerente extends Empleado{
+
+    public Gerente(String nombre,String apellido,char sexo,Domicilio domicilio, int dni, LocalDate fec_nacimiento, int nro_legajo, LocalDate fec_ingreso) {
+        super(nombre,apellido,sexo,domicilio,dni,fec_nacimiento,nro_legajo,fec_ingreso,"Gerente");
+    }
+    
+}
