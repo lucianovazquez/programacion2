42a4dae71c88c323c2e1d8c6eb3c54e61962e75d
Roque <Roque@wolf-rg.lan>
2014-10-18 17:49:32 -0300

    Agregado Metodos para manejo de Jefes

--- Trabajo de Laboratorio/src/trabajodelaboratorio/Operario.java
+++ Trabajo de Laboratorio/src/trabajodelaboratorio/Operario.java
@@ -6,6 +6,7 @@
 
 package trabajodelaboratorio;
 
+import excepciones.YaTieneUnJefeException;
 import java.time.LocalDate;
 
 /**
@@ -13,9 +14,46 @@ import java.time.LocalDate;
  * @author Net
  */
 public class Operario extends Empleado{
+    private Jefe jefe;
 
     public Operario(String nombre, String apellido,char sexo, Domicilio domicilio, int dni, LocalDate fec_nacimiento, int nro_legajo, LocalDate fec_ingreso) {
         super(nombre,apellido,sexo,domicilio,dni,fec_nacimiento,nro_legajo,fec_ingreso,"Operario");
     }
     public Operario(){}
+    
+    /**
+     * @return the jefe
+     */
+    public Jefe getJefe() {
+    return jefe;
+    }
+
+    /**
+     * @param jefe the jefe to set
+     */
+    
+    public void addJefe(Jefe jefe) throws YaTieneUnJefeException{
+        
+        if(this.jefe==null){
+            this.jefe = jefe;
+        }
+        else{
+            throw new YaTieneUnJefeException("Ya tiene un jefe asignado");
+        }
+    }
+    
+    /**
+     * @param jefe the jefe to set
+     */
+    public void deleteJefe(){
+        
+        this.jefe = null;
+    }
+    
+    /**
+     * @param jefe the jefe to set
+     */
+    public void setJefe(Jefe jefe){
+        this.jefe = jefe;
+    }
 }
