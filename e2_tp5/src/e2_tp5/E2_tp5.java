5cf9d3d83d10292691cacb539fbd92a2e82bc804
lucianovazquez <lucianovazquez@outlook.com>
2014-10-22 17:18:40 -0300

    Ejercico 2 - Trabajo Práctico 5

---
+++ e2_tp5/src/e2_tp5/E2_tp5.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package e2_tp5;
+
+/**
+ *
+ * @author Luciano
+ */
+public class E2_tp5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Formulario f = new Formulario();
+        f.setVisible(true);
+    }
+    
+}
